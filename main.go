cd34d4876f03f1924854a27c982d77a9754bc6e6
keijiyoshida <keijiyoshida.mail@gmail.com>
2016-10-02 14:24:58 -0500

    add a stack

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"io"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type scanner struct {
@@ -24,15 +26,28 @@ func (sc *scanner) nextInt() int {
 	return i
 }
 
-func newScanner(r io.Reader) *scanner {
+func newScanner(r io.Reader, split bufio.SplitFunc) *scanner {
 	sc := bufio.NewScanner(r)
-	sc.Split(bufio.ScanWords)
+	sc.Split(split)
 	return &scanner{
 		sc: sc,
 	}
 }
 
-var sc = newScanner(os.Stdin)
+var wsc = newScanner(os.Stdin, bufio.ScanWords)
+var lsc = newScanner(os.Stdin, bufio.ScanLines)
+
+func next() string {
+	return wsc.next()
+}
+
+func nextInt() int {
+	return wsc.nextInt()
+}
+
+func nextLine() string {
+	return lsc.next()
+}
 
 func max(x, y int) int {
 	if x > y {
@@ -48,5 +63,37 @@ func min(x, y int) int {
 	return y
 }
 
-func main() {
+func joinInts(a []int, sep string) string {
+	b := make([]string, len(a))
+	for i, v := range a {
+		b[i] = strconv.Itoa(v)
+	}
+	return strings.Join(b, sep)
 }
+
+type stringStack struct {
+	stack []string
+}
+
+func (ss *stringStack) push(s string) {
+	ss.stack = append(ss.stack, s)
+}
+
+func (ss *stringStack) pop() (string, error) {
+	l := len(ss.stack)
+
+	if l == 0 {
+		return "", errors.New("Empty stack")
+	}
+
+	s := ss.stack[l-1]
+	ss.stack = ss.stack[:l-1]
+
+	return s, nil
+}
+
+func newStringStack(l, c int) *stringStack {
+	return &stringStack{make([]string, l, c)}
+}
+
+func main() {}
