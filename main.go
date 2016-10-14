9083f86cc486c6f392320e2ba8863566f1d885e2
keijiyoshida <keijiyoshida.mail@gmail.com>
2016-10-14 16:39:46 +0900

    Removed the scanner and added a buffered reader.

--- main.go
+++ main.go
@@ -2,50 +2,75 @@ package main
 
 import (
 	"bufio"
-	"io"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type scanner struct {
-	sc *bufio.Scanner
+type bufReader struct {
+	r   *bufio.Reader
+	buf []byte
+	i   int
 }
 
-func (sc *scanner) next() string {
-	sc.sc.Scan()
-	return sc.sc.Text()
+var reader = &bufReader{
+	bufio.NewReader(os.Stdin),
+	make([]byte, 0),
+	0,
 }
 
-func (sc *scanner) nextInt() int {
-	i, err := strconv.Atoi(sc.next())
+func (r *bufReader) readLine() {
+	if r.i < len(r.buf) {
+		return
+	}
+	r.buf = make([]byte, 0)
+	r.i = 0
+	for {
+		line, isPrefix, err := r.r.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		r.buf = append(r.buf, line...)
+		if !isPrefix {
+			break
+		}
+	}
+}
+
+func (r *bufReader) next() string {
+	r.readLine()
+	from := r.i
+	for ; r.i < len(r.buf); r.i++ {
+		if r.buf[r.i] == ' ' {
+			break
+		}
+	}
+	s := string(r.buf[from:r.i])
+	r.i++
+	return s
+}
+
+func (r *bufReader) nextLine() string {
+	r.readLine()
+	s := string(r.buf[r.i:])
+	r.i = len(r.buf)
+	return s
+}
+
+func next() string {
+	return reader.next()
+}
+
+func nextInt() int {
+	i, err := strconv.Atoi(reader.next())
 	if err != nil {
 		panic(err)
 	}
 	return i
 }
 
-func newScanner(r io.Reader, split bufio.SplitFunc) *scanner {
-	sc := bufio.NewScanner(r)
-	sc.Split(split)
-	return &scanner{
-		sc: sc,
-	}
-}
-
-var wsc = newScanner(os.Stdin, bufio.ScanWords)
-var lsc = newScanner(os.Stdin, bufio.ScanLines)
-
-func next() string {
-	return wsc.next()
-}
-
-func nextInt() int {
-	return wsc.nextInt()
-}
-
 func nextLine() string {
-	return lsc.next()
+	return reader.nextLine()
 }
 
 func max(x, y int) int {
